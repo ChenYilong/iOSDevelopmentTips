9ea4760aa132a89f1eb51ee124b8e8c4066dd411
chenyilong <luohanchenyilong@163.com>
2015-10-15 17:28:27 +0800

    update

---
+++ Demo1_ ProtobufParseForObjectiveCDemo/ProtobufParseForObjectiveCDemo/ProtobufParseForObjectiveCDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProtobufParseForObjectiveCDemo
+//
+//  Created by 陈宜龙 on 15/10/14.
+//  Copyright © 2015年 http://weibo.com/luohanchenyilong/ 微博@iOS程序犭袁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
